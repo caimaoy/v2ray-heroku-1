06290dddd6bad3efd9dd1974614f17cbaa55fadb
yeahwu <yeahwu@users.noreply.github.com>
2020-01-16 20:51:11 +0800

    Update configure.sh
    Add DNS over HTTPS with for v2ray.

--- configure.sh
+++ configure.sh
@@ -28,7 +28,17 @@ cat <<-EOF > /etc/v2ray/config.json
     "protocol": "freedom",
     "settings": {}
   }
-  ]
+  ],
+  "dns": {
+      "servers": [
+          "https+local://1.1.1.1/dns-query",
+          "1.1.1.1",
+          "223.5.5.5",
+          "8.8.8.8",
+          "119.29.29.29",
+          "localhost"
+      ]
+  }
 }
 EOF
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
